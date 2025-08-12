84ea76f17a392e873728f655de3cf84b765f478b
kylertk21 <kylertk2102@gmail.com>
2025-08-11 23:12:27 -0600

    Updated pin numbers for sda & scl

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from machine import Pin, I2C
+import machine
 from oled_lib import SSD1306_I2C
 from moisture_lib import read_moisture
 from light_lib import BH1750
 import time
 import network, urequests
-SENSOR_ID= 1
+SENSOR_ID= "Basil"
 API_URL = "http://192.168.0.19:5000/api/sensor"
 
 SSID = "Die_Online"
@@ -12,7 +13,7 @@ PASSWORD = "AKConquest420!"
 wlan = network.WLAN(network.STA_IF)
 ip_address = wlan.ifconfig()[0]
 
-i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+i2c = I2C(0, scl=Pin(21), sda=Pin(22))
 oled_width = 128
 oled_height = 64
 oled = SSD1306_I2C(oled_width, oled_height, i2c)
@@ -47,7 +48,7 @@ def post_data(water_value, sunlight_value):
             "sensor_id": SENSOR_ID,
                 "readings": {
                 "water": water_value,
-                "sunlight": sunlight_value
+                "sunlight": sunlight_value,
             }
         }
         headers = {'Content-Type': 'application/json'}
@@ -75,4 +76,6 @@ while True:
     moisture = read_moisture()
     post_success = post_data(moisture, light)
     populate_screen(moisture, light, wifi_connected, post_success)
-    time.sleep(10)
+
+    sleep_time = 600 * 1000 #10 mins
+    machine.deepsleep(sleep_time)
